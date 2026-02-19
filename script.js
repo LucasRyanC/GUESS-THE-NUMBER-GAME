3031859645a957b8d4ccfcd8ccb62fd552c6d851
Lucas Ryan <108099420+LucasRyanC@users.noreply.github.com>
2026-02-19 12:10:45 +0000

    Add files

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🏯 Correct Number!';
+
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
+  } else {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+//reset the game
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
